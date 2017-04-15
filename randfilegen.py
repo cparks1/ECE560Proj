d813ab13484c23f30fc22bd5d3aa21d22a565a74
Chris <cparks13@live.com>
2017-04-15 12:51:07 -0400

    Created script file that will randomly generate (using a seed) 5 text files of varying sizes.
    Signed-off-by: Chris <cparks13@live.com>

---
+++ randfilegen.py
@@ -0,0 +1,4 @@
+# Title: randfilegen.py
+# Description: To fulfill our project requirements, this python script will require the user to specify a directory
+# and then randomly generate text files of the following sizes: 1 KB, 100 KB, 1 MB, 100 MB, and 500 MB. These files
+# will be stored in the given directory, along with a text file that specifies the file path of each file.
