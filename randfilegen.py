0ad73d785308dc4fd03f82c954cd29a0f445b0d3
Chris <cparks13@live.com>
2017-04-15 14:59:40 -0400

    randfilegen.py script was placing generated files in script's working directory. It now asks the user for the directory, and places the files in the given directory.
    Signed-off-by: Chris <cparks13@live.com>

--- randfilegen.py
+++ randfilegen.py
@@ -1,24 +1,26 @@
-import random # Used for random generation
-import string # Used to grab character sets containing 0-9, A-Z, a-z
-import os # Used to get current working directory
+import random  # Used for random generation
+import string  # Used to grab character sets containing 0-9, A-Z, a-z, and conversion of integers to strings.
 
 # Title: randfilegen.py
 # Description: To fulfill our project requirements, this python script will require the user to specify a directory
 # and then randomly generate text files of the following sizes: 1 KB, 100 KB, 1 MB, 100 MB, and 500 MB. These files
 # will be stored in the given directory, along with a text file that specifies the file path of each file.
 
-sizes = [1000, 100000, 1000000, 100000000, 500000000] # Definition of the file sizes.
-r = random.seed(1234) # Set a seed so files generated in the future are identical.
-cwd = os.getcwd()
+sizes = [1000, 100000, 1000000]#, 100000000, 500000000] # Definition of the file sizes.
+r = random.seed(1234)  # Set a seed so files generated in the future are identical.
+
+cwd = input("Define save directory: ")  # Ask the user to define where we'll save the randomly generated text files.
+cwd = cwd.replace('\\', '/')  # If the user gave response in format "C:\Users\..." reformat it to "C:/Users/..."
+if cwd.endswith('/'):  # If the user added '/' at the end, remove it so we know what to expect.
+    cwd = cwd[:-1]
 
 for filesize in sizes:
-    filepath = cwd + str(filesize) + '.txt'
-    try: # Attempt to open file path.
+    filepath = cwd + '/' + str(filesize) + '.txt'  # Define the path of the file being worked on.
+    try:  # Attempt to open file path.
         file = open(filepath, 'w')
-    except: # If there is a failure in opening the file path, notify the user.
+    except:  # If there is a failure in opening the file path, notify the user.
         print('Unexpected error. Could not randomly generate a text file at "'+filepath+'".')
-    else: # If opening the file was successful, begin generation.
-        for i in range(0, filesize): # Generate random characters (0-9,A-Z,a-z) and write to file.
+    else:  # If opening the file was successful, begin generation.
+        for i in range(0, filesize):  # Generate random characters (0-9,A-Z,a-z) and write to file.
             file.write(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits))
-        file.close() # Close the file, as we are finished.
-        
+        file.close()  # Close the file, as we are finished.
